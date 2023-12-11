9eeb60272309c05a634d8e1fbe5e521c9ab38ccb
joshdotjs <joshdotjs@gmail.com>
2023-12-11 06:35:56 -0600

    form

--- web/src/pages/ContactPage/ContactPage.jsx
+++ web/src/pages/ContactPage/ContactPage.jsx
@@ -1,5 +1,5 @@
 import { MetaTags } from '@redwoodjs/web'
-import { Form, TextField, Submit } from '@redwoodjs/forms'
+import { Form, Label, TextField, TextAreaField, FieldError, Submit } from '@redwoodjs/forms'
 
 const ContactPage = () => {
   const onSubmit = (data) => {
@@ -16,7 +16,42 @@ const ContactPage = () => {
       </p>
 
       <Form onSubmit={onSubmit}>
-        <TextField name="input" />
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
         <Submit>Save</Submit>
       </Form>
     </>
