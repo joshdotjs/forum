15e584b2e98f4b36dd3329c3972a7fdcf36055ee
joshdotjs <joshdotjs@gmail.com>
2023-12-16 13:24:13 -0600

    threads cell

--- web/src/pages/ForumPage/ForumPage.jsx
+++ web/src/pages/ForumPage/ForumPage.jsx
@@ -1,19 +1,13 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import ThreadsCell from 'src/components/Thread/ThreadsCell'
 
 const ForumPage = () => {
   return (
     <>
       <MetaTags title="Forum" description="Forum page" />
 
-      <h1>ForumPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ForumPage/ForumPage.jsx</code>
-      </p>
-      <p>
-        My default route is named <code>forum</code>, link to me with `
-        <Link to={routes.forum()}>Forum</Link>`
-      </p>
+      <ThreadsCell />
     </>
   )
 }
