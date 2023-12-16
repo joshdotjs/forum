2b6cb138be795a574824fd6c1a5e1f39d1e1ca46
joshdotjs <joshdotjs@gmail.com>
2023-12-16 10:55:03 -0600

    login button in navbar

--- web/src/layouts/ForumLayout/ForumLayout.jsx
+++ web/src/layouts/ForumLayout/ForumLayout.jsx
@@ -1,39 +1,10 @@
-import { Link, routes } from '@redwoodjs/router'
-import { useAuth } from 'src/auth'
 import { Toaster } from '@redwoodjs/web/toast'
 import Navbar from "./ForumNavbar"
 
 const ForumLayout = ({ children }) => {
-
-  const { isAuthenticated, currentUser, logOut } = useAuth()
-
   return <>
     <Toaster />
-
-    <header>
-      <div className="flex-between">
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
-        {isAuthenticated ? (
-          <div>
-            <span>Logged in as {currentUser.email}</span>{' '}
-            <button type="button" onClick={logOut}>
-              Logout
-            </button>
-          </div>
-        ) : (
-          <Link to={routes.login()}>Login</Link>
-        )}
-      </div>
-
-      <Link to={routes.home()}>Home</Link>
-      <Link to={routes.about()}>About</Link>
-      <Link to={routes.contact()}>Contact</Link>
-    </header>
-
-    <Navbar {...{ logOut }} />
-
+    <Navbar />
     {children}
   </>
 }
--- web/src/layouts/ForumLayout/ForumNavbar.jsx
+++ web/src/layouts/ForumLayout/ForumNavbar.jsx
@@ -1,5 +1,6 @@
 import { Fragment } from 'react'
-import { NavLink, routes } from '@redwoodjs/router'
+import { useAuth } from 'src/auth'
+import { Link, NavLink, routes } from '@redwoodjs/router'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { PlusIcon } from '@heroicons/react/20/solid'
@@ -15,7 +16,9 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Navbar({ logOut }) {
+export default function Navbar() {
+
+  const { isAuthenticated, currentUser, logOut } = useAuth()
 
   const navigation = [
     { name: 'Home',    fn: () => routes.home()    },
@@ -48,14 +51,7 @@ export default function Navbar({ logOut }) {
                     )}
                   </Disclosure.Button>
                 </div>
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
+                <div className="hidden md:flex md:items-center md:space-x-4">
                   {navigation.map((item) => (
                     <NavLink
                       key={item.name}
@@ -72,65 +68,84 @@ export default function Navbar({ logOut }) {
                   ))}
                 </div>
               </div>
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <button
-                    type="button"
-                    className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
-                    <PlusIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
-                    New Job
-                  </button>
-                </div>
-                <div className="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
-                  <button
-                    type="button"
-                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  >
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
 
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">Open user menu</span>
-                        <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-200"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
+
+
+
+              {isAuthenticated ? (
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <button
+                      type="button"
+                      className="relative inline-flex items-center gap-x-1.5 rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                     >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
-                              <div
-                                onClick={item.fn}
-                                className={classNames(
-                                  active ? 'bg-gray-100' : '',
-                                  'block px-4 py-2 text-sm text-gray-700'
-                                )}
-                              >
-                                {item.name}
-                              </div>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
+                      <PlusIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
+                      New Thread
+                    </button>
+                  </div>
+                  <div className="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
+                    <button
+                      type="button"
+                      className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    >
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+
+                    {/* Profile dropdown */}
+                    <Menu as="div" className="relative ml-3">
+                      <div>
+                        <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                          <span className="absolute -inset-1.5" />
+                          <span className="sr-only">Open user menu</span>
+                          <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {userNavigation.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <div
+                                  onClick={item.fn}
+                                  className={classNames(
+                                    active ? 'bg-gray-100' : '',
+                                    'block px-4 py-2 text-sm text-gray-700'
+                                  )}
+                                >
+                                  {item.name}
+                                </div>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  </div>
                 </div>
-              </div>
+              ) : (
+                <div className="hidden md:flex md:items-center md:space-x-4">
+                  <Link
+                    to={routes.login()}
+                    className={classNames(
+                      'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'rounded-md px-3 py-2 text-sm font-medium'
+                    )}
+                  >
+                    Login
+                  </Link>
+                </div>
+              )}
+
             </div>
           </div>
 
