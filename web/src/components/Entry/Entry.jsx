b2b4660149c8bc11439620978e24555949f84699
joshDotJS <joshDotJS@gmail.com>
2023-12-19 12:31:05 -0600

    modal to add reply

--- web/src/components/Entry/Entry.jsx
+++ web/src/components/Entry/Entry.jsx
@@ -6,25 +6,27 @@ const formattedDate = (datetime) => {
 
 const Entry = ({ entry }) => {
   return (
-    <div
-      style={{
-        border: '1px solid red',
-        padding: '10px',
-        display: 'grid',
-        gridTemplateColumns: '1fr 3fr',
-        gridTemplateRows: 'auto 1fr',
-      }}
-    >
+    <>
+      <div
+        style={{
+          border: '1px solid red',
+          padding: '10px',
+          display: 'grid',
+          gridTemplateColumns: '1fr 3fr',
+          gridTemplateRows: 'auto 1fr',
+        }}
+      >
 
-      {/* <p>{JSON.stringify(entry)}</p> */}
-      <p style={{ border: 'solid black 1px', padding: '0.5rem', gridRow: '1 / -1' }}><b>User:</b>  {entry.user?.name}</p>
-      <div style={{ border: 'solid black 1px', padding: '0.5rem' }}>
-        {entry?.title && <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>Title:</b> {entry.title}</p> }
-        <p style={{ border: 'solid black 1px', padding: '0.5rem' }}>{formattedDate(entry.createdAt)}</p>
+        {/* <p>{JSON.stringify(entry)}</p> */}
+        <p style={{ border: 'solid black 1px', padding: '0.5rem', gridRow: '1 / -1' }}><b>User:</b>  {entry.user?.name}</p>
+        <div style={{ border: 'solid black 1px', padding: '0.5rem' }}>
+          {entry?.title && <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>Title:</b> {entry.title}</p> }
+          <p style={{ border: 'solid black 1px', padding: '0.5rem' }}>{formattedDate(entry.createdAt)}</p>
+        </div>
+
+        <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>Body:</b>  {entry.body}</p>
       </div>
-
-      <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>Body:</b>  {entry.body}</p>
-    </div>
+    </>
   )
 }
 
--- web/src/pages/ThreadPage/ThreadPage.jsx
+++ web/src/pages/ThreadPage/ThreadPage.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import { MetaTags } from '@redwoodjs/web'
 import ThreadCell from 'src/components/Thread/ThreadCell'
 import RepliesCell from 'src/components/RepliesCell'
+import Modal from './Modal'
 
 const Container = ({ children }) => (
   <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">{ children }</div>
@@ -8,11 +10,21 @@ const Container = ({ children }) => (
 
 const ThreadPage = ({ id }) => {
 
+  const [open, setOpen] = useState(true)
+
   return (
     <Container>
       <MetaTags title="Thread" description="Thread page" />
+      <button
+        type="button"
+        className="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        onClick={() => setOpen(true)}
+      >
+        Button text
+      </button>
       <ThreadCell id={id} />
       <RepliesCell />
+      <Modal {...{ open, setOpen }} />
     </Container>
   )
 }
