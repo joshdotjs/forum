35b84b017b9192f1470c7490a9c387b4290d35b7
joshdotjs <joshdotjs@gmail.com>
2023-12-19 06:56:13 -0600

    display thread

--- web/src/components/ThreadComp/ThreadComp.jsx
+++ web/src/components/ThreadComp/ThreadComp.jsx
@@ -10,15 +10,20 @@ const ThreadComp = ({ thread }) => {
         style={{
           border: '1px solid red',
           padding: '10px',
+          display: 'grid',
+          gridTemplateColumns: '1fr 3fr',
+          gridTemplateRows: '1fr 1fr',
         }}
       >
 
         {/* <p>{JSON.stringify(thread)}</p> */}
-        <p><b>Title:</b> {thread.title}</p>
-        <p><b>Body:</b>  {thread.body}</p>
-        <p><b>UserId:</b>  {thread.userId}</p>
-        <p><b>CreatedAt:</b>  {thread.createdAt}</p>
-        <p><b>User:</b>  {thread.user.name}</p>
+        <p style={{ border: 'solid black 1px', padding: '0.5rem', gridRow: '1 / -1' }}><b>User:</b>  {thread.user.name}</p>
+        <div style={{ border: 'solid black 1px', padding: '0.5rem' }}>
+          <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>Title:</b> {thread.title}</p>
+          <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>CreatedAt:</b>  {thread.createdAt}</p>
+        </div>
+
+        <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>Body:</b>  {thread.body}</p>
       </div>
     </>
   )
