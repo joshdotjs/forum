92e2b853a1cee7e9b36e30535e9ded9bc92a37f5
joshDotJS <joshDotJS@gmail.com>
2023-12-19 12:07:27 -0600

    format the date

--- web/src/components/Entry/Entry.jsx
+++ web/src/components/Entry/Entry.jsx
@@ -1,3 +1,9 @@
+const formattedDate = (datetime) => {
+  const parsedDate = new Date(datetime)
+  const month = parsedDate.toLocaleString('default', { month: 'long' })
+  return `${parsedDate.getDate()} ${month} ${parsedDate.getFullYear()}`
+}
+
 const Entry = ({ entry }) => {
   return (
     <div
@@ -6,7 +12,7 @@ const Entry = ({ entry }) => {
         padding: '10px',
         display: 'grid',
         gridTemplateColumns: '1fr 3fr',
-        gridTemplateRows: '1fr 1fr',
+        gridTemplateRows: 'auto 1fr',
       }}
     >
 
@@ -14,7 +20,7 @@ const Entry = ({ entry }) => {
       <p style={{ border: 'solid black 1px', padding: '0.5rem', gridRow: '1 / -1' }}><b>User:</b>  {entry.user?.name}</p>
       <div style={{ border: 'solid black 1px', padding: '0.5rem' }}>
         {entry?.title && <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>Title:</b> {entry.title}</p> }
-        <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>CreatedAt:</b>  {entry.createdAt}</p>
+        <p style={{ border: 'solid black 1px', padding: '0.5rem' }}>{formattedDate(entry.createdAt)}</p>
       </div>
 
       <p style={{ border: 'solid black 1px', padding: '0.5rem' }}><b>Body:</b>  {entry.body}</p>
