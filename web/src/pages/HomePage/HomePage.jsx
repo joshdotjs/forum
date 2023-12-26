5f6a6baf5ebb771e14fa014e770bb98c2e4f46cb
joshdotjs <joshdotjs@gmail.com>
2023-12-25 18:19:02 -0600

    update

--- web/src/pages/HomePage/HomePage.jsx
+++ web/src/pages/HomePage/HomePage.jsx
@@ -21,6 +21,15 @@ const HomePage = () => {
         <h1 className="mb-2">Coming Soon</h1>
         <h1 className="mb-2">Launching Jan 2024</h1>
         <h1 className="">(we will use a faster database once launched)</h1>
+
+        <div>
+          <ul>
+            <li><strong>Features Coming Soon:</strong></li>
+            <li>Direct messages,</li>
+            <li>Real time chatroom via WebSockets,</li>
+            <li>Thread categories</li>
+          </ul>
+        </div>
       </div>
     </>
   )
