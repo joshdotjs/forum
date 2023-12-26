d1a82a8022974ca6320559e03c29d1e307a28bed
joshdotjs <joshdotjs@gmail.com>
2023-12-26 05:49:10 -0600

    footer

--- web/src/layouts/ForumLayout/ForumLayout.jsx
+++ web/src/layouts/ForumLayout/ForumLayout.jsx
@@ -1,12 +1,23 @@
 import { Toaster } from '@redwoodjs/web/toast'
 import Navbar from "./ForumNavbar"
+import Footer from "./ForumFooter"
 
 const ForumLayout = ({ children }) => {
-  return <>
-    <Toaster />
-    <Navbar />
-    {children}
-  </>
+  return (
+    <div
+      className="bg-gray-900 text-white"
+      style={{ // sticky footer
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Toaster />
+      <Navbar />
+      {children}
+      <Footer style={{ marginTop: 'auto' }} />
+    </div>
+  )
 }
 
 export default ForumLayout
