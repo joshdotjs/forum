d8e656293075d419e7b065af4859c262ea284aa5
joshdotjs <joshdotjs@gmail.com>
2023-12-27 17:09:42 -0600

    responsive

--- web/src/components/Entry/Entry.jsx
+++ web/src/components/Entry/Entry.jsx
@@ -1,4 +1,4 @@
-import { formatDate, formatTime } from "src/lib/formatters"
+import { truncate, formatDate, formatTime } from "src/lib/formatters"
 
 const Entry = ({ entry }) => {
   return (
@@ -15,7 +15,26 @@ const Entry = ({ entry }) => {
       >
 
         {/* <p>{JSON.stringify(entry)}</p> */}
-        <p className="border-gray-800 border-r-2" style={{ padding: '0.5rem', gridRow: '1 / -1' }}>— {entry.user?.name}</p>
+        <p
+          className="border-gray-800 border-r-2"
+          style={{
+            padding: '0.5rem',
+            gridRow: '1 / -1',
+            minWidth: '120px',
+          }}
+        >
+          {/* — {truncate(entry.user?.name, 50)} */}
+          <span className="hidden lg:inline">— </span>
+          {/* <span className="inline sm:hidden">{truncate('01234567890123456789', 10)}</span> */}
+          {/* <span className="hidden sm:inline md:hidden">{truncate('01234567890123456789', 12)}</span> */}
+          {/* <span className="hidden md:inline lg:hidden">{truncate('01234567890123456789', 16)}</span> */}
+          {/* <span className="hidden lg:inline">{truncate('01234567890123456789', 20)}</span> */}
+          <span className="inline sm:hidden">{truncate(entry.user.name, 10)}</span>
+          <span className="hidden sm:inline md:hidden">{truncate(entry.user.name, 12)}</span>
+          <span className="hidden md:inline lg:hidden">{truncate(entry.user.name, 16)}</span>
+          <span className="hidden lg:inline">{truncate(entry.user.name, 20)}</span>
+
+        </p>
 
         <div style={{ padding: '0.5rem' }}>
           {entry?.title && <p className="mb-2"><b>Title:</b> {entry.title}</p> }
